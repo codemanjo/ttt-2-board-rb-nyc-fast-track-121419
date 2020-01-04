c3d2294c0580587b86eb2286e5fd7e8e6ca31e40
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-04 20:40:39 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,1 @@
-
-#Define the variable board below.
-row = ["0", "1", "2"]
-#position 1: Left
-  row[0]
-
-  #position 2: Middle
-  row[1]
-
-#position 3: Right
-row[2
-
-#move x to the Position 2, Middle
-row[1] = "x"
+row = ["0","1","2"]# Define the variable board below.
