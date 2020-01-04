9dcf19436d7c1bb51db917f474f5a145e22d10c4
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-04 20:21:41 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,14 @@
-# Define the variable board below.
+
+#Define the variable board below.
+row = ["0", "1", "2"]
+#position 1: Left
+  row[0]
+
+  #position 2: Middle
+  row[1]
+
+#position 3: Right
+row[2
+
+#move x to the Position 2, Middle
+row[1] = "x"
